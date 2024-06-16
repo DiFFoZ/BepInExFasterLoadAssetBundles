bc85ce9c9c299bd07d8d242ce14bfc71fe334220
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-06-16 15:23:57 +0700

    Fix check of decompress flag failing with diff version

--- BepInExFasterLoadAssetBundles/Helpers/BundleHelper.cs
+++ BepInExFasterLoadAssetBundles/Helpers/BundleHelper.cs
@@ -8,8 +8,16 @@ internal static class BundleHelper
     public static bool CheckBundleIsAlreadyDecompressed(Stream stream)
     {
         // special thanks to AssetRipper for providing info how bundle header are serialized
-        // https://github.com/AssetRipper/AssetRipper/blob/master/Source/AssetRipper.IO.Files/BundleFiles/CompressionType.cs
-        stream.Seek(0x2d, SeekOrigin.Begin);
+        // https://github.com/AssetRipper/AssetRipper/blob/master/Source/AssetRipper.IO.Files/BundleFiles/BundleHeader.cs
+        // https://github.com/AssetRipper/AssetRipper/blob/master/Source/AssetRipper.IO.Files/BundleFiles/FileStream/FileStreamBundleHeader.cs
+        stream.Seek(0x0, SeekOrigin.Begin);
+
+        // skip magic string
+        SkipString(stream);
+        stream.Position += 4; // skip version
+        SkipString(stream); // skip web version
+        SkipString(stream); // skip web min rev
+        stream.Position += 8 + 4 + 4; // skip size, compressed block size, uncompressed block size
 
         Span<byte> buffer = stackalloc byte[4];
         stream.Read(buffer);
@@ -22,5 +30,14 @@ internal static class BundleHelper
         // 2 - LZ4
         // 3 - LZ4HC
         return compressionType is 0 or 2;
+    }
+
+    private static void SkipString(Stream stream)
+    {
+        // "C" string type (zero-term and the end of string)
+        while (stream.ReadByte() != 0)
+        {
+            continue;
+        }
     }
 }
