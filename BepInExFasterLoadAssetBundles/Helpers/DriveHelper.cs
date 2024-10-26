e899bb3c2e7b50f2b57ecc3ed9f5116da475e91f
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-10-26 20:24:35 +0700

    Potential fix of Wine reporting wrong free disk space

--- BepInExFasterLoadAssetBundles/Helpers/DriveHelper.cs
+++ BepInExFasterLoadAssetBundles/Helpers/DriveHelper.cs
@@ -8,6 +8,10 @@ internal static class DriveHelper
         var driveLetter = Path.GetPathRoot(Path.GetFullPath(path));
         var driveInfo = new DriveInfo(driveLetter);
 
-        return driveInfo.AvailableFreeSpace > (expectedSpaceGB * FileHelper.c_GBToBytes);
+        // Switched to TotalFreeSpace for potential fix for Wine users.
+        // AvailableFreeSpace uses user disk quota to get accurate free space
+        // and probably because of that Wine reports invalid available free space.
+
+        return driveInfo.TotalFreeSpace > (expectedSpaceGB * FileHelper.c_GBToBytes);
     }
 }
