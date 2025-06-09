f5f439cb46130c23dd785dee1b722c9fdd892deb
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2025-06-09 18:17:17 +0700

    Add ignore space check arg

--- BepInExFasterLoadAssetBundles/Helpers/DriveHelper.cs
+++ BepInExFasterLoadAssetBundles/Helpers/DriveHelper.cs
@@ -1,10 +1,26 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace BepInExFasterLoadAssetBundles.Helpers;
+
 internal static class DriveHelper
 {
+    private static readonly bool s_IgnoreDriveSpace;
+
+    static DriveHelper()
+    {
+        s_IgnoreDriveSpace = Environment.GetCommandLineArgs()
+            .Contains("--ignore-space-check", StringComparer.OrdinalIgnoreCase);
+    }
+
     public static bool HasDriveSpaceOnPath(string path, long expectedSpaceGB)
     {
+        if (s_IgnoreDriveSpace)
+        {
+            return true;
+        }
+
         var driveLetter = Path.GetPathRoot(Path.GetFullPath(path));
         var driveInfo = new DriveInfo(driveLetter);
 
