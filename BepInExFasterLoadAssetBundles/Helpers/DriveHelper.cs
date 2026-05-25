cf871c42de59d9b7a83494df1d3b73d32f5ef6d3
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2026-05-26 01:55:49 +0700

    Use internal method for getting disk free space

--- BepInExFasterLoadAssetBundles/Helpers/DriveHelper.cs
+++ BepInExFasterLoadAssetBundles/Helpers/DriveHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using HarmonyLib;
 
 namespace BepInExFasterLoadAssetBundles.Helpers;
 
@@ -8,10 +10,16 @@ internal static class DriveHelper
 {
     private static readonly bool s_IgnoreDriveSpace;
 
+    private delegate void GetDiskFreeSpace(string path, out ulong availableFreeSpace, out ulong totalSize, out ulong totalFreeSpace);
+    private static readonly GetDiskFreeSpace s_GetDiskFreeSpace;
+
     static DriveHelper()
     {
         s_IgnoreDriveSpace = Environment.GetCommandLineArgs()
             .Contains("--ignore-space-check", StringComparer.OrdinalIgnoreCase);
+
+        var method = typeof(DriveInfo).GetMethod("GetDiskFreeSpace", BindingFlags.NonPublic | BindingFlags.Static);
+        s_GetDiskFreeSpace = AccessTools.MethodDelegate<GetDiskFreeSpace>(method);
     }
 
     public static bool HasDriveSpaceOnPath(string path, long expectedSpaceGB)
@@ -21,13 +29,10 @@ internal static class DriveHelper
             return true;
         }
 
-        var driveLetter = Path.GetPathRoot(Path.GetFullPath(path));
-        var driveInfo = new DriveInfo(driveLetter);
+        path = Path.GetFullPath(path);
 
-        // Switched to TotalFreeSpace for potential fix for Wine users.
-        // AvailableFreeSpace uses user disk quota to get accurate free space
-        // and probably because of that Wine reports invalid available free space.
+        s_GetDiskFreeSpace(path, out var availableFreeSpace, out var totalSize, out var totalFreeSpace);
 
-        return driveInfo.TotalFreeSpace > (expectedSpaceGB * FileHelper.c_GBToBytes);
+        return availableFreeSpace > (ulong)(expectedSpaceGB * FileHelper.c_GBToBytes);
     }
 }
